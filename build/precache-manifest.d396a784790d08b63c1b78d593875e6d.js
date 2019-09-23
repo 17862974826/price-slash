7227e2503732f7af7d6c00c7832c6e55bef23f65
罗童 <luotong@luotongdeMacBook-Pro.local>
2019-09-23 23:02:32 +0800

    add build file

---
+++ build/precache-manifest.d396a784790d08b63c1b78d593875e6d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114f1b066437ec7831038a1f2de293a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13059a3fb28c8b790700",
+    "url": "/static/css/main.d03d3025.chunk.css"
+  },
+  {
+    "revision": "2b23852e91e7f11c723e",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "13059a3fb28c8b790700",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "75a0686b338fc25bfe4e",
+    "url": "/static/js/runtime~main.js"
+  },
+  {
+    "revision": "d8c324e5d043ccc88f8f393c6194de1f",
+    "url": "/static/media/about.d8c324e5.png"
+  },
+  {
+    "revision": "59cdaf76bbebe7c191edf54321286b1b",
+    "url": "/static/media/backDataList.59cdaf76.png"
+  },
+  {
+    "revision": "35acee47b877a3c7d9ed22bb57ed8941",
+    "url": "/static/media/backHome.35acee47.png"
+  },
+  {
+    "revision": "b280fbdb6acd89a970ab610173a6fb9b",
+    "url": "/static/media/character.b280fbdb.png"
+  },
+  {
+    "revision": "f5c8056cdef1575cbc3241f4c735cdf4",
+    "url": "/static/media/feature.f5c8056c.png"
+  },
+  {
+    "revision": "beca0bd4e20d9dcad13481bed2824555",
+    "url": "/static/media/headerBg.beca0bd4.png"
+  },
+  {
+    "revision": "d4521af806e779739423b0435a824d65",
+    "url": "/static/media/icon.d4521af8.png"
+  },
+  {
+    "revision": "dc83de0134fc075547fafeaf3846b356",
+    "url": "/static/media/logo.dc83de01.png"
+  },
+  {
+    "revision": "69554ab1d5535ff873efe4aa983fe6b7",
+    "url": "/static/media/me1.69554ab1.png"
+  },
+  {
+    "revision": "2971faa0e6ed6d9d0fe79e64b7cbf299",
+    "url": "/static/media/me2.2971faa0.png"
+  },
+  {
+    "revision": "850720db8fca7e2e33aba2134ba230cf",
+    "url": "/static/media/me3.850720db.png"
+  },
+  {
+    "revision": "0553106f5ad69300c88068c8b59dd569",
+    "url": "/static/media/whyBg.0553106f.png"
+  }
+]);
